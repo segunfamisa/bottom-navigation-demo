d125f08aa5a517a67c156b5da0acc3f42881b602
Segun Famisa <segunfamisa@gmail.com>
2016-10-23 18:30:42 +0100

    chore: Added Buttom Navigation View sample implementation

--- app/src/main/java/com/segunfamisa/sample/buttomnav/MainActivity.java
+++ app/src/main/java/com/segunfamisa/sample/buttomnav/MainActivity.java
@@ -1,13 +1,107 @@
 package com.segunfamisa.sample.buttomnav;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.ColorRes;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String SELECTED_ITEM = "arg_selected_item";
+
+    private BottomNavigationView mButtomNav;
+    private int mSelectedItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mButtomNav = (BottomNavigationView) findViewById(R.id.navigation);
+        mButtomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                selectFragment(item);
+                return true;
+            }
+        });
+
+        MenuItem selectedItem;
+        if (savedInstanceState != null) {
+            mSelectedItem = savedInstanceState.getInt(SELECTED_ITEM, 0);
+            selectedItem = mButtomNav.getMenu().findItem(mSelectedItem);
+        } else {
+            selectedItem = mButtomNav.getMenu().getItem(0);
+        }
+        selectFragment(selectedItem);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putInt(SELECTED_ITEM, mSelectedItem);
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onBackPressed() {
+        MenuItem homeItem = mButtomNav.getMenu().getItem(0);
+        if (mSelectedItem != homeItem.getItemId()) {
+            // select home item
+            selectFragment(homeItem);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    private void selectFragment(MenuItem item) {
+        Fragment frag = null;
+        // init corresponding fragment
+        switch (item.getItemId()) {
+            case R.id.menu_home:
+                frag = MenuFragment.newInstance(getString(R.string.text_home),
+                        getColorFromRes(R.color.color_home));
+                break;
+            case R.id.menu_notifications:
+                frag = MenuFragment.newInstance(getString(R.string.text_notifications),
+                        getColorFromRes(R.color.color_notifications));
+                break;
+            case R.id.menu_search:
+                frag = MenuFragment.newInstance(getString(R.string.text_search),
+                        getColorFromRes(R.color.color_search));
+                break;
+        }
+
+        // update selected item
+        mSelectedItem = item.getItemId();
+
+        // uncheck the other items.
+        for (int i=0; i<mButtomNav.getMenu().size(); i++) {
+            MenuItem menuItem = mButtomNav.getMenu().getItem(i);
+            menuItem.setChecked(menuItem.getItemId() == item.getItemId());
+        }
+
+        updateToolbarText(item.getTitle());
+
+        if (frag != null) {
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.add(R.id.container, frag, frag.getTag());
+            ft.commit();
+        }
+    }
+
+    private void updateToolbarText(CharSequence text) {
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setTitle(text);
+        }
+    }
+
+    private int getColorFromRes(@ColorRes int resId) {
+        return ContextCompat.getColor(this, resId);
     }
 }
---
+++ app/src/main/java/com/segunfamisa/sample/buttomnav/MenuFragment.java
@@ -0,0 +1,72 @@
+package com.segunfamisa.sample.buttomnav;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+/**
+ * Fragment class for each nav menu item
+ */
+public class MenuFragment extends Fragment {
+    private static final String ARG_TEXT = "arg_text";
+    private static final String ARG_COLOR = "arg_color";
+
+    private String mText;
+    private int mColor;
+
+    private View mContent;
+    private TextView mTextView;
+
+    public static Fragment newInstance(String text, int color) {
+        Fragment frag = new MenuFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_TEXT, text);
+        args.putInt(ARG_COLOR, color);
+        frag.setArguments(args);
+        return frag;
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_menu, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // retrieve text and color from bundle or savedInstanceState
+        if (savedInstanceState == null) {
+            Bundle args = getArguments();
+            mText = args.getString(ARG_TEXT);
+            mColor = args.getInt(ARG_COLOR);
+        } else {
+            mText = savedInstanceState.getString(ARG_TEXT);
+            mColor = savedInstanceState.getInt(ARG_COLOR);
+        }
+
+        // initialize views
+        mContent = view.findViewById(R.id.fragment_content);
+        mTextView = (TextView) view.findViewById(R.id.text);
+
+        // set text and background color
+        mTextView.setText(mText);
+        mContent.setBackgroundColor(mColor);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        outState.putString(ARG_TEXT, mText);
+        outState.putInt(ARG_COLOR, mColor);
+        super.onSaveInstanceState(outState);
+    }
+}
